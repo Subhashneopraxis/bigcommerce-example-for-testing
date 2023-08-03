2fbb607f56aa32f71fe0aba97cb0272e43cbae78
Subhashneopraxis <subash.surendran@neopraxis.in>
2023-08-03 17:31:20 +0530

    edit config file

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/myhomepage:path*",
+        source: "/api/plasmic-host:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
