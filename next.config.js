8ad7fc3b97c487a08e52c2b9141f93284db2c394
Subhashneopraxis <subash.surendran@neopraxis.in>
2023-08-03 17:01:38 +0530

    edit config file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
   // has some troubles with it. See
   // https://github.com/adobe/react-spectrum/labels/strict%20mode
   reactStrictMode: false,
+   
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/plasmic-host:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+
 };
 
 module.exports = nextConfig;
